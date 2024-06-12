d898df523fbfc96f9e59d1f605993138a20d804e
kambojsaab300 <50055925+kambojsaab300@users.noreply.github.com>
2024-06-12 16:16:07 +0000

    conversiono perations

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,3 +1,15 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+
 // number => 2 to power 53
 // bigint => use big number
 // string => ""

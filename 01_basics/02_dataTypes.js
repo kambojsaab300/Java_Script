45c64b876ed3d2a604676c3c9fae2c6d93d8f452
kambojsaab300 <50055925+kambojsaab300@users.noreply.github.com>
2024-06-12 05:38:20 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+// number => 2 to power 53
+// bigint => use big number
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined
+// Symbol => unique
+
+
+// object =>
+
+    console.log(typeof undefined) //undefined
+    console.log(typeof null) //object
+
+
+
+    // Ecma Script
+    // https://tc39.es/ecma262/
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,21 @@
-console.log("Mohit")
+const userId = 454545 // no value change
+let userEmail = "mohit@gmail.com" //
+var userPassword = "93434"   // not use  (If I use the variable again {userPassword} all the old values ​​will change)
+ 
+let userState;
+
+// var not understand scope {} so that (Therefore it is not used)
+
+
+
+/*
+prefer not to use var
+beacuse of issue in block scope and function scope
+*/
+
+// userId = 4er433 not change
+
+userEmail = "raman@gmail.com"
+userPassword = "000000"
+
+console.table([userId, userEmail, userPassword, userState])

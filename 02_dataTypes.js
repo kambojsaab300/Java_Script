bd815469f72744e854918255f05b2c965f016913
kambojsaab300 <50055925+kambojsaab300@users.noreply.github.com>
2024-06-18 21:00:37 +0530

    01_basics
    01_basics javascript

---
+++ 02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+
+// number => 2 to power 53
+// bigint => use big number
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined
+// Symbol => unique
+
+
+// object =>
+
+    console.log(typeof undefined) //undefined
+    console.log(typeof null) //object
+
+
+
+    // Ecma Script
+    // https://tc39.es/ecma262/
---
+++ 06nums_and_math.js
@@ -0,0 +1,47 @@
+const score = 300
+
+// console.log(score)
+
+const balance = new Number(100);
+
+// console.log(balance.toString().match);
+// console.log(balance.toFixed(2));  // use ecommerce application add .00 number
+
+const otherNumber = 23.80454
+
+// console.log(otherNumber.toPrecision(4)); // 23.80 value show limat
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-IN')); // 10,00,000 india value
+
+
+//  q 
+// number is data type 
+
+// ******************** Math *******************
+
+
+
+// console.log(Math)
+// console.log(Math.abs(-4)) // + convert - to + value = 4
+// console.log(Math.round(4.4)) // 4.5 upper value 5 (4.4 down value 4 ) = 4
+// console.log(Math.ceil(3.2)) // 3.2, 3.3 and value select = 4
+// console.log(Math.floor(5.5)) // 5.4, 5.5, 5.6 and value select = 5
+// console.log(Math.min(4, 5, 2, 5)) // find number min value = 2
+// console.log(Math.max(4, 5, 2, 5)) // find number max value = 5
+
+// console.log(Math.random()) //  0.1 value
+// console.log((Math.random()*10)) // 9 value swift left (0.3434 = 3.84989)
+// console.log(Math.floor(Math.random()*10) + 10) //  
+// console.log(Math.random()) //  0.1 value
+
+const min = 10
+const max = 20
+
+// console.log(Math.floor(Math.random() * (max - min + 1 ) + max))
+console.log((Math.random() * (max - min + 1) + max)) // min 10 
+console.log((Math.random() * (max - min + 1) + min)) // max 20 
+
+
+
